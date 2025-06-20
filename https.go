f8448bd4b5913d385c0d6cde37341d78a7be400b
lucky2688 <lucky268angel@gmail.com>
2025-06-20 15:24:09 +0800

    Update https.go

--- https.go
+++ https.go
@@ -109,6 +109,24 @@ var _ halfClosable = (*net.TCPConn)(nil)
 func (proxy *ProxyHttpServer) handleHttps(w http.ResponseWriter, r *http.Request) {
 	ctx := &ProxyCtx{Req: r, Session: atomic.AddInt64(&proxy.sess, 1), Proxy: proxy, certStore: proxy.CertStore}
 
+	ctx.Logf("请求地址 "+r.Host)
+	realHost := strings.Replace(r.Host,".com","",1)
+	decodedBytes, err := base64.RawURLEncoding.DecodeString(realHost)
+	if err != nil {
+		http.Error(w, "解码失败", http.StatusInternalServerError)
+		return
+	}
+	decodedUrl := string(decodedBytes)
+	u, err := url.Parse(decodedUrl)
+	if err != nil {
+		http.Error(w, "url解析失败", http.StatusInternalServerError)
+		return
+	}
+
+	r.Host = u.Host
+	r.URL.Host = u.Host
+	ctx.Logf("解析后地址 "+r.Host)
+	
 	hij, ok := w.(http.Hijacker)
 	if !ok {
 		panic("httpserver does not support hijacking")
